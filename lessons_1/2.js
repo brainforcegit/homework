fee92edde903b38aecf6f9ef4ec3c2786281de6c
BrainForce <brainforceceo@gmail.com>
2024-02-19 12:56:05 +0000

    FIX:lessons_2

--- lessons_1/2.js
+++ lessons_1/2.js
@@ -1,6 +1,7 @@
-for (let i = 0; i <= 100; i++) {
-  if (i % 2 === 0) {
-    console.log(i)
+nextPrime:
+  for (let i = 2; i <= 100; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+    console.log(i);
   }
-}
-
