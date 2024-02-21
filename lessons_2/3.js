aaa3df8c4a262ae869ef330e1f5d90d6c4226a22
BrainForce <brainforceceo@gmail.com>
2024-02-21 23:00:00 +0000

    if to hasOwnProperty

--- lessons_2/3.js
+++ lessons_2/3.js
@@ -3,8 +3,7 @@ const array = [1, 2, 3, 3, 3, 4, 2, 5, 5, 6, 6, 1, 1, 9, 9, 10];
 const groupByRepeating = (array) => {
   return array.reduce((res, current) => {
     const key = current
-
-    if (!res[key]) {
+    if (!res.hasOwnProperty(key)) {
       res[key] = 0
     }
     res[key] += 1
@@ -13,4 +12,4 @@ const groupByRepeating = (array) => {
   }, {})
 }
 
-groupByRepeating(array);
+console.log(groupByRepeating(array));
