d6daddef1e393743b4a77ad244dbebe204376f3a
BrainForce <brainforceceo@gmail.com>
2024-02-22 00:00:03 +0000

    FEAAT:findBigRepeatingNumber
    findBigRepeatingNumber from array

---
+++ lessons_2/6.js
@@ -0,0 +1,27 @@
+const array = [1, 2, 3, 3, 3, 4, 2, 5, 5, 6, 6, 1, 1, 9, 9, 10, 10, 10, 10];
+
+const groupByRepeating = (array) => {
+  return array.reduce((res, current) => {
+    if (!res.hasOwnProperty(current)) {
+      res[current] = 0
+    }
+    res[current] += 1
+
+    return res
+  }, {})
+}
+
+const findBigRepeatingNumber = (array) => {
+  let maxNum = 0;
+  let objRepeating = groupByRepeating(array)
+  let maxRepeatingNumber = Math.max(...Object.values(objRepeating));
+  let objKeys = Object.keys(objRepeating)
+  for (let i = 0; i < objKeys.length; i++) {
+    maxNum = (objRepeating[objKeys[i]] === maxRepeatingNumber && objKeys[i] > maxNum) ? objKeys[i] : maxNum
+
+  }
+
+  return maxNum
+}
+
+console.log(findBigRepeatingNumber(array));
