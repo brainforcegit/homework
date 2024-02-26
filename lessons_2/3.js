51d9a1bd69e704f052b7d28419e2057c435247ed
BrainForce <brainforceceo@gmail.com>
2024-02-27 02:51:05 +0400

    FIX:comment lessons 2
    without reduce please

--- lessons_2/3.js
+++ lessons_2/3.js
@@ -1,16 +1,31 @@
 // without reduce please
 const array = [1, 2, 3, 3, 3, 4, 2, 5, 5, 6, 6, 1, 1, 9, 9, 10];
 
-const groupByRepeating = (array) => {
-  return array.reduce((res, current) => {
-    const key = current
-    if (!res.hasOwnProperty(key)) {
-      res[key] = 0
-    }
-    res[key] += 1
+/**
+ const groupByRepeating = (array) => {
+ return array.reduce((res, current) => {
+ const key = current
+ if (!res.hasOwnProperty(key)) {
+ res[key] = 0
+ }
+ res[key] += 1
 
-    return res
-  }, {})
+ return res
+ }, {})
+ }
+
+ console.log(groupByRepeating(array));
+ */
+
+const  groupByRepeating = (array) => {
+  const res = {}
+  for (let i = 0; i < array.length; i++) {
+    if (!res[array[i]]) {
+      res[array[i]] = 0
+    }
+    res[array[i]] += 1
+  }
+  return res
 }
 
-console.log(groupByRepeating(array));
+console.log(groupByRepeating(array))
