a85423d8b3588ae10f702fd9f97c644304e112a3
BrainForce <brainforceceo@gmail.com>
2024-02-27 03:02:48 +0400

    FIX: lessons_2-5
     use decstructuring not function

--- lessons_2/5.js
+++ lessons_2/5.js
@@ -1,16 +1,20 @@
 const object = {aa: 8, a: {b: {c: {d: {e: {k: 7333}}}}}};
 // use destructuring not function
-function getValueByKey(obj, key) {
-  let objects = [];
-  for (let i in obj) {
-    if (!obj.hasOwnProperty(i)) continue;
-    if (typeof obj[i] == 'object') {
-      objects = objects.concat(getValueByKey(obj[i], key));
-    } else if (i === key) {
-      objects.push(obj[i]);
-    }
-  }
-  return objects;
-}
+/**
+ function getValueByKey(obj, key) {
+ let objects = [];
+ for (let i in obj) {
+ if (!obj.hasOwnProperty(i)) continue;
+ if (typeof obj[i] == 'object') {
+ objects = objects.concat(getValueByKey(obj[i], key));
+ } else if (i === key) {
+ objects.push(obj[i]);
+ }
+ }
+ return objects;
+ }
 
-console.log(getValueByKey(object, "k"))
+ console.log(getValueByKey(object, "k"))
+ */
+const {a: {b: {c: {d: {e: {k:value}}}}}} = object;
+console.log(value)
