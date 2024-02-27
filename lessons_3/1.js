fa08159cfaeb5650a34a5a2fdef4637f89edf9c2
BrainForce <brainforceceo@gmail.com>
2024-02-27 05:46:25 +0400

    FIX:set prototype for custom new

--- lessons_3/1.js
+++ lessons_3/1.js
@@ -1,7 +1,8 @@
-Function.prototype.customNew =  function (...args){
-    const instance = {};
-    const result = this.apply(instance, [...args]);
-    return result instanceof Object ? result : instance;
+Function.prototype.customNew = function (...args) {
+  const instance = {};
+  Object.setPrototypeOf(instance, this.prototype);
+  const result = this.apply(instance, [...args]);
+  return result instanceof Object ? result : instance;
 }
 
 function ExampleConstructor(name, surname) {
@@ -10,7 +11,10 @@ function ExampleConstructor(name, surname) {
   return `${this.name} ${this.surname}`
 }
 
-const newObj = new ExampleConstructor('Object New',"Surname");
+const newObj = new ExampleConstructor('Object New', "Surname");
+const customNewObj = ExampleConstructor.customNew("Custom New", "Surname")
 console.log(newObj)
-console.log(ExampleConstructor.customNew("Custom New", "Surname"));
+console.log(customNewObj)
+console.log(newObj instanceof ExampleConstructor)
+console.log(customNewObj instanceof ExampleConstructor);
 
