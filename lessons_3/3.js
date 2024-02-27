3b9d7a37d1a5c35b5c1ea9a2cdfe45f9166ac2a9
BrainForce <brainforceceo@gmail.com>
2024-02-27 05:19:32 +0400

    FEAT:logs
    1log-10
    2log - typeerror
    3log - window
    4log - undefined

---
+++ lessons_3/3.js
@@ -0,0 +1,28 @@
+// var a = 10;
+//
+// function foo() {
+//   console.log(this.a)
+// }
+// foo()
+//
+//
+// "use strict"
+// var a = 10;
+//
+// function foo() {
+//   console.log(this.a)
+// }
+// foo()
+//
+
+// function foo() {
+//   console.log(this)
+// }
+// foo()
+
+// "use strict"
+//
+// function foo() {
+//   console.log(this)
+// }
+// foo()
