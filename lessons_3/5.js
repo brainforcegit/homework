985b13471ea4e489796bc39553f8fe6533a89f0d
BrainForce <brainforceceo@gmail.com>
2024-02-28 15:20:15 +0400

    FIX:TODO check this function logic, I give array with 12 length but i get array with 10 length

--- lessons_3/5.js
+++ lessons_3/5.js
@@ -1,12 +1,14 @@
-const array = [2, 4, 1, 5, 7, 9]
+const array = [2, 4, 1, 5, 7, 8,10,12,9, 55, 89, 22]
 
 function rotate(array) {
   let newArray = [...array]
   const sortArray = []
-  for (let i = 0; i < newArray.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     newArray = newArray.filter(item => !sortArray.includes(item))
-    sortArray.push(Math.max(...newArray))
-    sortArray.push(Math.min(...newArray))
+    if(newArray.length){
+      sortArray.push(Math.max(...newArray))
+      sortArray.push(Math.min(...newArray))
+    }
   }
   return sortArray
 }
