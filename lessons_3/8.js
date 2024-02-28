380e9f19f7f81fe59fb862028de3c208e9d4e2f0
BrainForce <brainforceceo@gmail.com>
2024-02-28 15:13:12 +0400

    FIX:TODO please check this function, if in object not found key you should return default value

--- lessons_3/8.js
+++ lessons_3/8.js
@@ -1,6 +1,6 @@
-const get = (obj, keyPath) =>
+const get = (obj = {}, keyPath, defaultValue) =>
   keyPath.split('.').reduce((acc, key) =>
-    (acc && acc[key] !== undefined) ? acc[key] : undefined, obj)
+    (acc && acc[key] !== undefined) ? acc[key] : defaultValue, obj)
 
 const obj = {a: {b: 2}};
-console.log(get(obj, 'a.b'))
+console.log(get(obj, 'a.b.d', 10))
